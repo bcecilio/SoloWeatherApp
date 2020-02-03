605e003162e624e5e7fbaeeab10e74b1182c66be
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-03 15:11:46 -0500

    "created API client file"

---
+++ WeatherApp/WeatherApp/WeatherAPIClient.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherAPIClient.swift
+//  WeatherApp
+//
+//  Created by Brendon Cecilio on 2/3/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct WeatherAPIClient {
+    func getWeather(zipCodeQuery: String, completion: @escaping (Result<[Weather], AppError>) -> ()) {
+        let endpointURL = "https://api.darksky.net/forecast/9fb817c1de2067d841b72f0f5757717f/\(zipCodeQuery)"
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let data = try JSONDecoder().decode([Weather].self, from: data)
+                    let weatherData = data
+                    completion(.success(weatherData))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
